e7ba6a6f0d43fe2ff570aae88393303c37699620
Sarim Qureshi <52934393+N19htw1n9@users.noreply.github.com>
2020-10-15 00:31:17 -0500

    10/15 Commit - SQ

--- src/main/java/JavaFXTemplate.java
+++ src/main/java/JavaFXTemplate.java
@@ -3,64 +3,86 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class JavaFXTemplate extends Application {
-    private TextField t1;
+    private TextArea textBox; //Changed from TextField to TextArea for larger text
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Hello from JavaFX");
+    public void start(Stage primaryStage) throws Exception
+    {
+        primaryStage.setTitle("Keno");
 
-        Button b1 = new Button("button 1");
-        t1 = new TextField();
+        Button playGame = new Button("Play game");
+        textBox = new TextArea();
 
 
-        b1.setOnAction(e -> t1.setText("Message from button 1"));
+        playGame.setOnAction(e -> textBox.setText("Message from button 1"));
 
         MenuBar menu = new MenuBar();
-        Menu mOne = new Menu("option 1"); //a menu goes inside a menu bar
-        Menu mTwo = new Menu("option 2");
+        Menu mOne = new Menu("Menu");
 
-        MenuItem iOne = new MenuItem("click me");
-        iOne.setOnAction(e -> t1.setText("menu item was clicked"));
+        //Menu options need to be implemented to complete Step 1
+
+        MenuItem iOne = new MenuItem("Display the rules of the game");
+        iOne.setOnAction(e -> textBox.setText("Instructions"));
         mOne.getItems().add(iOne); //add menu item to first menu
 
-        menu.getMenus().addAll(mOne, mTwo);
+        MenuItem iTwo = new MenuItem("Display the odds of winning");
+        iTwo.setOnAction(e -> textBox.setText("Odds:"));
+        mOne.getItems().add(iTwo); //add menu item to first menu
+
+        MenuItem iThree = new MenuItem("Exit the game");
+        iThree.setOnAction(e -> textBox.setText("Exit"));
+        mOne.getItems().add(iThree); //add menu item to first menu
+
+        menu.getMenus().addAll(mOne);
 		
 		GridPane grid = new GridPane();
 		grid.setAlignment(Pos.CENTER);
 		addGrid(grid);
 
-        //new scene with root node
         Scene scene = new Scene(
                 new VBox(
                         20,
                         menu,
-                        b1,
-                        t1,
-                        grid
+                        playGame,
+                        textBox //Unsure of how to hide the textbox until a menu item has been selected
                 ),
-                700,
-                700
+                1050,
+                950
         );
         primaryStage.setScene(scene); //set the scene in the stage
         primaryStage.show(); //make visible to the user
+
+
+        //Since the scene for the game is different,
+        // I commented this out until we implement the EventHandler for the 'playGame' button
+        /*Scene GameScene = new Scene(
+                new VBox(
+                        20,
+                        menu,
+                        b1,
+                        textBox,
+                        grid
+                ),
+                800,
+                800
+        );
+        primaryStage.setScene(scene); //set the scene in the stage
+        primaryStage.show(); //make visible to the user*/
     }
 
     /**
-     * Genereates a 4x4 grid and adds buttons
+     * Generates a 4x4 grid and adds buttons
      * @param grid
      */
     public void addGrid(GridPane grid) {
@@ -70,7 +92,7 @@ public class JavaFXTemplate extends Application {
                 Button b = new Button(Integer.toString(counter));
                 int finalX = x;
                 int finalI = i;
-                b.setOnAction(e -> t1.setText("Hello from button[" + finalX + ", " + finalI + "]"));
+                b.setOnAction(e -> textBox.setText("Hello from button[" + finalX + ", " + finalI + "]"));
                 grid.add(b, i, x);
                 counter++;
             }
