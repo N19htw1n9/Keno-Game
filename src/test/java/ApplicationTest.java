2a7d18d6eba2bafd4eea44ee58a92c8dc5e299db
ayaanqui <moahammedayaan.dev@gmail.com>
2020-10-19 11:26:04 -0500

    Removed package declaration and redundant imports

--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -1,14 +1,9 @@
-package test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 import Objects.DrawRandom;
 import Objects.UserPick;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.TreeSet;
 import java.util.Iterator;
