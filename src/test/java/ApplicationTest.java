8ef8064dedff6e5edeadfa2b7221f5ba14ca690c
Sarim Qureshi <52934393+N19htw1n9@users.noreply.github.com>
2020-10-19 15:13:49 -0500

    Some Test Cases

--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -1,8 +1,9 @@
-package Test;
+package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import Objects.DrawRandom;
+import Objects.UserPick;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,8 @@ import java.util.Iterator;
 public class ApplicationTest
 {
     private DrawRandom rand;
+    private UserPick up;
+    private UserPick upVar;
 
     @BeforeAll
     void init()
@@ -29,10 +32,27 @@ public class ApplicationTest
         Iterator<Integer> it = ts.iterator();
 
         assertEquals(ts.size(), 10);
+
         while(it.hasNext())
         {
             assertTrue((it.next() >= 1) && (it.next() <= 20));
         }
+    }
+
+    @Test
+    void UserPickTest()
+    {
+        upVar = new UserPick(3);
+        upVar.setNumber(21);
+        upVar.setNumber(12);
+        upVar.setNumber(56);
+        assertEquals(upVar.getNumbers().size(), 2);
+        assertEquals(upVar.getNumbers().get(0), 21);
+        assertEquals(upVar.getNumbers().get(0), 12);
+
+        up = new UserPick();
+
+
     }
 
 
