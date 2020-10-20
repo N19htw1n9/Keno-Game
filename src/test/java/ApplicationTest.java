ade24ee3b6a008f440df8469fe3df5c1d4a6053b
Sarim Qureshi <52934393+N19htw1n9@users.noreply.github.com>
2020-10-19 22:34:19 -0500

    Unit Tests completed, 1 build failure & 1 test failure to be fixed V3

--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -1,58 +1,50 @@
 import Objects.DrawRandom;
 import Objects.UserPick;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.Test;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.*;
 import java.util.TreeSet;
 import java.util.Iterator;
 
-public class ApplicationTest
-{
-    private static DrawRandom rand;
+public class ApplicationTest {
     private DrawRandom rand;
     private UserPick up;
     private UserPick upVar;
 
     @BeforeAll
-    static void init()
-    void init()
-    {
+    void init() {
         rand = new DrawRandom(20, 10, 2);
     }
 
     @Test
-    public void DrawRandomTest()
-    void DrawRandomTest()
-    {
+    public void DrawRandomTest() {
         TreeSet<Integer> ts = rand.draw();
         Iterator<Integer> it = ts.iterator();
 
-        assertEquals(ts.size(), 10, "t");
-        assertEquals(rand.getMax(), 20, "t");
-        assertEquals(rand.getDraws(), 10, "t");
-        assertEquals(rand.getNumDraws(), 2, "t");
+        assertEquals(ts.size(), 10);
+        assertEquals(rand.getMax(), 20);
+        assertEquals(rand.getDraws(), 10);
+        assertEquals(rand.getNumDraws(), 2);
 
         rand.setMax(30);
         rand.setDraws(12);
         rand.setNumDraws(3);
-        assertEquals(rand.getMax(), 30, "t");
-        assertEquals(rand.getDraws(), 12, "t");
-        assertEquals(rand.getNumDraws(), 3, "t");
+        assertEquals(rand.getMax(), 30);
+        assertEquals(rand.getDraws(), 12);
+        assertEquals(rand.getNumDraws(), 3);
         assertEquals(ts.size(), 10);
 
-        while(it.hasNext())
-        {
-            assertTrue((it.next() >= 1) && (it.next() <= 20), "t");
+        while (it.hasNext()) {
+            assertTrue((it.next() >= 1) && (it.next() <= 20));
             assertTrue((it.next() >= 1) && (it.next() <= 20));
         }
     }
 
     @Test
-    public void UserPickTest()
-    void UserPickTest()
-    {
+    public void UserPickTest() {
         upVar = new UserPick(3);
         upVar.setNumber(21);
         assertEquals(upVar.getNumbers().size(), 2);
@@ -70,12 +62,12 @@ public class ApplicationTest
         Set<Integer> duplicateElements = new HashSet<>();
 
         for (Integer element : up.getNumbers())
-            if(!set.add(element))
+            if (!set.add(element))
                 duplicateElements.add(element);
 
         assertTrue(duplicateElements.isEmpty());
 
-        for(int k = 0; k < up.getNumbers().size(); k++)
+        for (int k = 0; k < up.getNumbers().size(); k++)
             assertTrue((up.getNumbers().get(k) <= 80) && (up.getNumbers().get(k) >= 1));
 
         up.setSpots(15);
@@ -87,12 +79,12 @@ public class ApplicationTest
         Set<Integer> duplicateElements2 = new HashSet<>();
 
         for (Integer element : up.getNumbers())
-            if(!set2.add(element))
+            if (!set2.add(element))
                 duplicateElements2.add(element);
 
         assertTrue(duplicateElements2.isEmpty());
 
-        for(int k = 0; k < up.getNumbers().size(); k++)
+        for (int k = 0; k < up.getNumbers().size(); k++)
             assertTrue((up.getNumbers().get(k) <= 80) && (up.getNumbers().get(k) >= 1));
 
         assertEquals(up.getSpots(), 15);
@@ -104,6 +96,7 @@ public class ApplicationTest
         up.setNumbers(list);
         assertEquals(up.getNumbers().size(), 3);
 
-        for(int k = 0; k < up.getNumbers().size(); k++)
+        for (int k = 0; k < up.getNumbers().size(); k++)
             assertEquals(up.getNumbers().get(k), list.get(k));
     }
+}
